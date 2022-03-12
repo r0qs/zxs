0e1fac634b2639d90e550da8058775a29792ae4c
Racin Nygaard <racin.nygaard@gmail.com>
2022-03-12 02:48:07 +0100

    Build Xapian script

--- build-xapian.sh
+++ build-xapian.sh
@@ -4,11 +4,12 @@ TAG=${TAG:-v1.4.19}
 git clone https://github.com/xapian/xapian
 cd xapian
 git checkout $TAG -b $TAG 
-./bootstrap xapian-core
-./configure CXXFLAGS=-O0 --disable-backend-honey --disable-backend-inmemory --disable-backend-remote
+./bootstrap
+./configure CXXFLAGS=-O --disable-backend-honey --disable-backend-inmemory --disable-backend-remote
 make -j$THREADS
 make -j$THREADS distclean
 cd xapian-core
 emconfigure ./configure CPPFLAGS='-DFLINTLOCK_USE_FLOCK' CXXFLAGS='-Oz -s USE_ZLIB=1 -fno-rtti' --disable-backend-honey --disable-backend-inmemory --disable-shared --disable-backend-remote
 emmake make -j$THREADS
+cp .libs/libxapian.a .libs/libxapian-1.5.a
 cd ../..
