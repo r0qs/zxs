6fcef38763c3a798971ea56ccc1f67af09c5b348
Racin Nygaard <racin.nygaard@gmail.com>
2022-03-15 12:58:45 +0100

    Search work. Add GetRandomArticle function.

--- demo/main.js
+++ demo/main.js
@@ -2,7 +2,7 @@ var BeeZIMSearcher = function() {
 	this.fulltextIndex = "./X/fulltext/xapian";
 	this.titleIndex = "./X/title/xapian"; // TODO: Can not load both indexes at the same time due to variables are overwritten in the global namespace.
 	this.dataDir = "./X";
-	this.files = [];
+	this.articles = [];
 	this.initRan = false;
 	this.xapian = new XapianAPI();
 	this.maxResults = 20;
@@ -20,18 +20,28 @@ BeeZIMSearcher.prototype.init = function(index) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET","files.json");
 	xhr.onload = function() {
-		self.files = JSON.parse(xhr.response);
+		self.parseFiles(xhr.response);
 	};
 	xhr.send();
 }
 
 BeeZIMSearcher.prototype.parseFiles = function(filesResponse) {
-	var self = this;
-	self.files.push({title:"prestley", url:"A/Elvis_presley"})
-	self.files.push({title:"pelvis", url:"A/Elvis_presley"})
+	let files = JSON.parse(filesResponse);
+	for (const [key, value] of Object.entries(files)){
+		if (key.startsWith("A/")){
+			this.articles.push(value);
+		}
+	}
 }
 
-BeeZIMSearcher.prototype.search = function(query) {
+BeeZIMSearcher.prototype.GetRandomArticle = function() {
+	if (!this.initRan) {
+		return "You need to run 'init()' before searching!";
+	}
+	return this.articles[this.articles.length * Math.random() << 0];
+}
+
+BeeZIMSearcher.prototype.Search = function(query) {
 	if (!this.initRan) {
 		return "You need to run 'init()' before searching!";
 	}
@@ -40,29 +50,36 @@ BeeZIMSearcher.prototype.search = function(query) {
 	let queryLower = query.toLowerCase();
 
 	this.xapian.queryXapianIndex(query,0,this.maxResults-this.titleResults).forEach((r) => {
-		results.push(r);
+		results.push({
+			docid: r.docid,
+			data: r.data,
+			wordcount: parseInt(this.xapian.getStringValue(r.docid,1)),
+			title: this.xapian.getStringValue(r.docid,0)
+		});
 	});
 	let wantedTitleMatch = this.maxResults-results.length;
-	for (const [key, value] of Object.entries(this.files)){
+	let titleResults = [];
+	for (let i = 0; i < this.articles.length; i++) {
 		if (wantedTitleMatch <= 0)
 			break;
-		let curr = this.files[i];
-		// console.log(key);
-		// console.log(value);
-		if(value.Metadata.Title.indexOf(queryLower) > -1) {
-			results.unshift({
+		let value = this.articles[i];
+		if(value.Metadata.Title.toLowerCase().indexOf(queryLower) > -1) {
+			titleResults.push({
 				query: query,
 				title: value.Metadata.Title,
-				data: key//value.Path
+				data: value.Path
 			});
 			wantedTitleMatch--;
 		}
 	}
+	titleResults.sort(function(a,b) {
+		return a.title.length - b.title.length;
+	});
 
-	
-	for(var i = 0; i < this.files.length; i++){
-		
+	// Top 3 results are from titleResults
+	if (titleResults.length > this.titleResults){
+		return titleResults.slice(0,this.titleResults).concat(results,titleResults.slice(this.titleResults))
 	}
-	
-	return results;
+
+	return titleResults.concat(results);
 }
